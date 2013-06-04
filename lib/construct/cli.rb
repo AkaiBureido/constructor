e74505f0b4ae41adfe76ebfe50dba3865f1b759a
AkaiBureido <utkin.oleg@me.com>
2013-06-03 22:20:14 -0700

    Refactored the file processing.

--- lib/construct/cli.rb
+++ lib/construct/cli.rb
@@ -14,7 +14,7 @@
 #===============================================================================
 # REFACTORING PLAN:
 # 1. Move the file detection stuff into the library [DONE]
-# 2. Move the file processing into the library
+# 2. Move the file processing into the library [DONE]
 #===============================================================================
 
 # REFACTORING ZONES WILL BE DEPICTED THIS WAY:
@@ -43,57 +43,7 @@ module Construct
 				# Getting the full path to the template 
 				requested_template_path = File.join TEMPLATE_BASE_PATH, *template_offset_path
 
-				#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-				# [2.] Construct::construct requested_template_path {{{
-				#
-				# there are a few possibilities now
-				# the template can be:
-				# * or a plain file with instructions
-				# * a directory
-				# * a zip file
-				template_type = nil
-				if File.exists? requested_template_path
-					template_type = :instruction_file
-				elsif Dir.exists? requested_template_path
-					template_type = :directory
-				elsif File.exists? requested_template_path + ".zip"
-					template_type = :zip_ball
-				else
-					puts "Error: the requested template #{requested_template_path} does not exist!"
-					exit
-				end
-
-				# Now that we know the file type leets act accordingly
-				case template_type
-				when :instruction_file
-					# TODO: Figure out syntax
-					#
-					# Assumptions to make coding easier for now:
-					# * indents are 4 space wide.
-					#
-					# Every thing that follows # is considered a comment
-
-					File.open( requested_template_path , "r" ) { |template_file|
-						# get all lines and delete empty ones
-						contents = (template_file.read.split("\n").delete_if { |a| a.empty? })
-
-						# First line must contain type in form of '# Type:<type>'
-						# Type acn be either:
-						# * constructor - my own script language for this task
-						# * shell       - self explanatory really
-						script_type = contents.shift.match(%r{Type:\s*(\w+)})[1].to_sym
-
-						# TODO: Write both shell and constructo implementations
-					}
-
-				when :directory
-					# TODO: Simply copy the directory
-				when :zip_ball
-					# TODO: Unpack the zip into folder
-				end
-				# }}}
-				#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+				Construct::FileProcessor.construct requested_template_path
 				# case "~~~~~~~"
 				# TODO: Implement:
 				# add    - adding a new template with editor, perhaps some cursing and black magic...
